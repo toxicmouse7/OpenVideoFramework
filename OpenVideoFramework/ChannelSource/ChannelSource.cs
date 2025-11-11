1b0d9633101629a63ad098a3f91c315c77ded163
Алексей Гладков <aleksej.s.gladkov@gmail.com>
2025-11-11 14:48:29 +0500

    feat(ChannelSource): implement channel source

---
+++ OpenVideoFramework/ChannelSource/ChannelSource.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Channels;
+using OpenVideoFramework.Pipelines;
+
+namespace OpenVideoFramework.ChannelSource;
+
+public class ChannelSource<TSource> : IPipelineSource<TSource>
+{
+    private readonly Channel<TSource> _source;
+
+    public ChannelSource(Channel<TSource> source)
+    {
+        _source = source;
+    }
+    
+    public Task PrepareForExecutionAsync(PipelineContext context, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task ProduceAsync(ChannelWriter<TSource> output, CancellationToken cancellationToken)
+    {
+        await foreach (var item in _source.Reader.ReadAllAsync(cancellationToken))
+        {
+            await output.WriteAsync(item, cancellationToken);
+        }
+    }
+}
